cf2fd878db82a36e8bf51123f50852fd4e3f6311
QuangLyHo <lyhoqq@gmail.com>
2022-12-22 13:43:28 -0500

    updated

--- scrimba_projects/react-tenzies-game/src/App.jsx
+++ scrimba_projects/react-tenzies-game/src/App.jsx
@@ -1,28 +1,57 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { nanoid } from 'nanoid'
 import Die from './Die'
+import Confetti from 'react-confetti'
 
 function App() {
-  const [dice, setDice] = useState(generateDice())
+  const [dice, setDice] = useState(getNewDice())
+  const [tenzies, setTenzies] = useState(false)
+
+  useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld)
+    const firstValue = dice[0].value
+    const sameValue = dice.every(die => die.value === firstValue)
+
+    if (allHeld && sameValue) {
+      setTenzies(true)
+    }
+  }, [dice])
 
   function generateDice() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+    }
+  }
+
+  function getNewDice() {
     const newDice = []
     for (let i = 0; i < 10; i++) {
-      let diceObj = {
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false,
-        id: nanoid()
-      }
-      newDice.push(diceObj)
+      newDice.push(generateDice())
     }
 
     return newDice
   }
 
-  function toggle(diceId) {
+  function holdDice(diceId) {
     setDice(prevDice => prevDice.map(die => {
-      return die.id === diceId ? {...die, isHeld: !die.isHeld} : die
+      return die.id === diceId ? 
+        {...die, isHeld: !die.isHeld} : die
     }))
+  }
+
+  function rollDice() {
+    if (!tenzies) {
+      setDice(prevDice => prevDice.map(die => {
+        return die.isHeld ? 
+          die : 
+          generateDice()
+      }))
+    } else {
+      setTenzies(false)
+      setDice(getNewDice())
+    }
   }
 
   const diceElement = dice.map(die => {
@@ -30,18 +59,24 @@ function App() {
       number={die.value} 
       key={die.id} 
       isHeld={die.isHeld} 
-      toggle={() => toggle(die.id)} 
+      toggle={() => holdDice(die.id)} 
       />
   })
   
   return (
     <main>
+      {tenzies && <Confetti />}
       <h1 className='title'>Tenzies</h1>
       <p className='subtitle'>Roll until all dice are the same. <span>Click each die to freeze it at its current value between rolls.</span></p>
       <div className='dice-container'>
         {diceElement}
       </div>
-      <button className='roll-btn'>Roll</button>
+      <button 
+        className='roll-btn' 
+        onClick={rollDice}
+      >
+        {tenzies ? 'New Game' : 'Roll'}
+      </button>
     </main>
   )
 }
